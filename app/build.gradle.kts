7df14afb04280762d14a0638ca79f194d17bae84
mohit <mohitgujjar2121@gmail.com>
2023-10-15 01:26:06 +0530

    added icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.mg.calculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mg.calculator"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
